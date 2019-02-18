468e99866cb900b3cccbaca60339551f18e90ff4
Alexandrerehder <47729125+Alexandrerehder@users.noreply.github.com>
2019-02-17 23:47:06 -0300

    Update logParse.java

--- ReadLog/src/demo/logParse.java
+++ ReadLog/src/demo/logParse.java
@@ -5,22 +5,16 @@ import java.nio.file.*;
 
 public class logParse {
 	
-	
-	
-	String info = " ";
-	
-	
-	
-	
 	public static void main(String[] args) {
 		
 		try {
-			List<String> lines = Files.readAllLines(Paths.get("src\\data\\file.log"));
+			List<String> lines = Files.readAllLines(Paths.get("src\\data\\file.log")); //Leitura do arquivo
 				for(String line : lines) {
-					line = line.replace("\"","");
+					line = line.replace("\"",""); //Declaração para trocar o argumento especificado por vírgula
 					
-					String[] result = line.split (" ");
+					String[] result = line.split (" "); //Argumento desejado para troca(onde existir espaço, faça a troca por vírgula)
 					for(String s : result)
+					//Apresentação
 					System.out.print(s + ", ");
 					System.out.println();
 					
@@ -34,3 +28,8 @@ public class logParse {
 		
 	}
 }
+
+//Minha tentativa baseada numa estrutura simples, de acordo com meu conhecimento básico da linguagem.
+//Gostaria de ter feito com outro argumento além de "espaço", para que as palavras finais não fossem separadas por vírgula,
+//além dos espaços preenchidos que não são necessários. 
+//Pensando em melhorias, implementaria uma forma de pesquisa para selecionar uma informação do log em específico.
